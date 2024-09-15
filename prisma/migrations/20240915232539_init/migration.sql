a18fc23c4a067bc693e1288a931f876cc7565455
NotNotRama <ramiroherrera.utn@gmail.com>
2024-09-15 23:29:06 +0000

    Clear db

--- prisma/migrations/20240915232539_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Message" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "author" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
